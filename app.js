ad7981ba0f863d0a2b4d1a35d3dcb5b88620fa3b
JoseAngelMartinezEnriquez <a181676@une.edu.mx>
2023-10-26 10:13:12 -0600

    Primer commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const { crearArchivo } = require('./helpers/multiplicar');
+const argv = require('./config/yargs');
+require('colors');
+
+// option('l')
+// listar
+// boolean
+// default: false
+
+console.clear();
+
+//const [ , , arg3 = 'base=5' ] = process.argv;
+//const [ , base = 5 ] = arg3.split('=');
+
+//const base = 5;
+
+crearArchivo(argv.b, argv.l, argv.h)
+    .then( nombreArchivo => console.log(nombreArchivo.rainbow, 'creado'))
+    .catch( err => console.log(err));
+
